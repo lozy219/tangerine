2e6c882e5d146120a7542604c07a14fb0868864e
Mingyu Lei <uygnim.iel@gmail.com>
2015-07-01 19:10:44 +0800

    upload page updated

---
+++ orange/app/scripts/controllers/upload.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name orangeApp.controller:UploadCtrl
+ * @description
+ * # UploadCtrl
+ * Controller of the orangeApp
+ */
+angular.module('orangeApp')
+	.controller('UploadCtrl', function ($scope) {
+
+	});

5fc4e1c6365faeeefa3728ea0588da936712b7e2
Mingyu Lei <uygnim.iel@gmail.com>
2015-07-14 17:06:22 +0800

    fix compatability of the past two parts of work

--- orange/app/scripts/controllers/home.js
+++ orange/app/scripts/controllers/home.js
@@ -138,21 +138,21 @@ angular.module('orangeApp')
 			return undefined;
 		};
 
-		setTimeout(function() {
-			$('.cell-arrow').on('click', function () {
-				var target = $(this).closest('.animate-cell');
-				if (target.hasClass('expanded-cell')) {
-					$('.expanded-cell').removeClass('expanded-cell');
-				} else {
-					$('.expanded-cell').removeClass('expanded-cell');
-					target.addClass('expanded-cell');
-				}
+		// setTimeout(function() {
+		// 	$('.cell-arrow').on('click', function () {
+		// 		var target = $(this).closest('.animate-cell');
+		// 		if (target.hasClass('expanded-cell')) {
+		// 			$('.expanded-cell').removeClass('expanded-cell');
+		// 		} else {
+		// 			$('.expanded-cell').removeClass('expanded-cell');
+		// 			target.addClass('expanded-cell');
+		// 		}
 				
-				if ($scope.isExpanded) {
-					setTimeout(function() {$('.masonry').masonry('layout');}, 5);
-				} else {
-					setTimeout(function() {$('.masonry').masonry('layout');}, 200);
-				}
-			});
-		}, 100);
+		// 		if ($scope.isExpanded) {
+		// 			setTimeout(function() {$('.masonry').masonry('layout');}, 5);
+		// 		} else {
+		// 			setTimeout(function() {$('.masonry').masonry('layout');}, 200);
+		// 		}
+		// 	});
+		// }, 100);
 	});
--- orange/app/scripts/controllers/project-cell.js
+++ orange/app/scripts/controllers/project-cell.js
@@ -23,7 +23,7 @@ angular.module('orangeApp')
 			}
 		);
 
-		$scope.toggleExpansion = function() {
+		$scope.toggleExpansion = function(event) {
 			if ($scope.$parent.expandedProject.id !== $scope.projectId) {
 				$scope.$parent.expandedProject.id = $scope.projectId;
 				$timeout(function() {
@@ -33,5 +33,21 @@ angular.module('orangeApp')
 				$scope.$parent.expandedProject.id = -1;
 				$scope.showReleases = false;
 			}
+
+			
+			var target = $(event.target).closest('.animate-cell');
+			if (target.hasClass('expanded-cell')) {
+				$('.expanded-cell').removeClass('expanded-cell');
+			} else {
+				$('.expanded-cell').removeClass('expanded-cell');
+				target.addClass('expanded-cell');
+			}
+			
+			if ($scope.isExpanded) {
+				setTimeout(function() {$('.masonry').masonry('layout');}, 5);
+			} else {
+				setTimeout(function() {$('.masonry').masonry('layout');}, 200);
+			}
+
 		};
 	});

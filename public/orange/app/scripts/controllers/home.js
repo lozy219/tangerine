22298ac52ba993060a350b2858c00c955269ccfb
Mingyu Lei <uygnim.iel@gmail.com>
2015-07-20 18:54:03 +0800

    prepare for redirect to download tomorrow

--- public/orange/app/scripts/controllers/home.js
+++ public/orange/app/scripts/controllers/home.js
@@ -9,13 +9,13 @@
  */
 angular.module('orangeApp')
 	.controller('HomeCtrl', ['$scope', 'Upload', '$http', function ($scope, Upload, $http) {
-		$http.get($scope.Constants.apiBaseUrl + 'full_project')
-			.success(function (data) {
-				$scope.projects = data;
-			})
-			.error(function (data) {
-				console.log(data);
-			});
+		// $http.get($scope.Constants.apiBaseUrl + 'full_project')
+		// 	.success(function (data) {
+		// 		$scope.projects = data;
+		// 	})
+		// 	.error(function (data) {
+		// 		console.log(data);
+		// 	});
 
 		$scope.$watch('uploadedFiles', function () {
 			console.log($scope.uploadedFiles);
@@ -42,118 +42,135 @@ angular.module('orangeApp')
 		// 	}
 		// };
 
-		// $scope.projects = [
-		// 	{
-		// 		id: 1,
-		// 		title: 'SampleStore',
-		// 		client: 'Singpost',
-		// 		release: {
-		// 			ios: [
-		// 				{
-		// 					id: 1,
-		// 					version: '1.8.3'
-		// 				},
-		// 				{
-		// 					id: 2,
-		// 					version: '1.8.5'
-		// 				},
-		// 				{
-		// 					id: 3,
-		// 					version: '1.8.6'
-		// 				},
-		// 				{
-		// 					id: 4,
-		// 					version: '1.8.7'
-		// 				},
-		// 				{
-		// 					id: 5,
-		// 					version: '1.9.0'
-		// 				}
-		// 			],
-		// 			android: [
-		// 				{
-		// 					id: 3,
-		// 					version: '2.3.6'
-		// 				},
-		// 				{
-		// 					id: 4,
-		// 					version: '1.2.1'
-		// 				}
-		// 			]
-		// 		}
-		// 	},
-		// 	{
-		// 		id: 2,
-		// 		title: 'TWG App',
-		// 		client: 'TWG',
-		// 		release: {
-		// 			ios: [
-		// 				{
-		// 					id: 1,
-		// 					version: '1.8.3'
-		// 				},
-		// 				{
-		// 					id: 2,
-		// 					version: '1.8.5'
-		// 				}
-		// 			],
-		// 			android: [
-		// 				{
-		// 					id: 3,
-		// 					version: '2.3.6'
-		// 				},
-		// 				{
-		// 					id: 4,
-		// 					version: '1.2.1'
-		// 				}
-		// 			]
-		// 		}
-		// 	},
-		// 	{
-		// 		id: 3,
-		// 		title: 'FastFast',
-		// 		client: 'Codigo',
-		// 		release: {
-		// 			ios: [
-		// 				{
-		// 					id: 1,
-		// 					version: '1.8.3'
-		// 				},
-		// 				{
-		// 					id: 2,
-		// 					version: '1.8.5'
-		// 				}
-		// 			],
-		// 			android: [
-		// 				{
-		// 					id: 3,
-		// 					version: '2.3.6'
-		// 				},
-		// 				{
-		// 					id: 4,
-		// 					version: '1.2.1'
-		// 				}
-		// 			]
-		// 		}
-		// 	},
-		// 	{
-		// 		id: 4,
-		// 		title: 'Singapore GP',
-		// 		client: 'Singapore grandprix',
-		// 		release: {
-		// 			ios: [
-		// 				{
-		// 					id: 1,
-		// 					version: '1.8.3'
-		// 				},
-		// 				{
-		// 					id: 2,
-		// 					version: '1.8.5'
-		// 				}
-		// 			]
-		// 		}
-		// 	},
-		// ];
+		$scope.projects = [
+			{
+				id: 1,
+				title: 'SampleStore',
+				client: 'Singpost',
+				release: {
+					ios: [
+						{
+							id: 1,
+							version: '1.8.3',
+							link: 'https://github.com/lozy219'
+						},
+						{
+							id: 2,
+							version: '1.8.5',
+							link: 'https://github.com/lozy219'
+						},
+						{
+							id: 3,
+							version: '1.8.6',
+							link: 'https://github.com/lozy219'
+						},
+						{
+							id: 4,
+							version: '1.8.7',
+							link: 'https://github.com/lozy219'
+						},
+						{
+							id: 5,
+							version: '1.9.0',
+							link: 'https://github.com/lozy219'
+						}
+					],
+					android: [
+						{
+							id: 3,
+							version: '2.3.6',
+							link: 'https://github.com/lozy219'
+						},
+						{
+							id: 4,
+							version: '1.2.1',
+							link: 'https://github.com/lozy219'
+						}
+					]
+				}
+			},
+			{
+				id: 2,
+				title: 'TWG App',
+				client: 'TWG',
+				release: {
+					ios: [
+						{
+							id: 1,
+							version: '1.8.3',
+							link: 'https://github.com/lozy219'
+						},
+						{
+							id: 2,
+							version: '1.8.5',
+							link: 'https://github.com/lozy219'
+						}
+					],
+					android: [
+						{
+							id: 3,
+							version: '2.3.6',
+							link: 'https://github.com/lozy219'
+						},
+						{
+							id: 4,
+							version: '1.2.1',
+							link: 'https://github.com/lozy219'
+						}
+					]
+				}
+			},
+			{
+				id: 3,
+				title: 'FastFast',
+				client: 'Codigo',
+				release: {
+					ios: [
+						{
+							id: 1,
+							version: '1.8.3',
+							link: 'https://github.com/lozy219'
+						},
+						{
+							id: 2,
+							version: '1.8.5',
+							link: 'https://github.com/lozy219'
+						}
+					],
+					android: [
+						{
+							id: 3,
+							version: '2.3.6',
+							link: 'https://github.com/lozy219'
+						},
+						{
+							id: 4,
+							version: '1.2.1',
+							link: 'https://github.com/lozy219'
+						}
+					]
+				}
+			},
+			{
+				id: 4,
+				title: 'Singapore GP',
+				client: 'Singapore grandprix',
+				release: {
+					ios: [
+						{
+							id: 1,
+							version: '1.8.3',
+							link: 'https://github.com/lozy219'
+						},
+						{
+							id: 2,
+							version: '1.8.5',
+							link: 'https://github.com/lozy219'
+						}
+					]
+				}
+			},
+		];
 
 		$scope.expandedProject = {
 			id: -1

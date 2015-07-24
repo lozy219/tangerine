74815f506a6565ec0cc80042640f813d95bc1842
Mingyu Lei <uygnim.iel@gmail.com>
2015-07-24 19:29:48 +0800

    base ui structure for client side project

--- public/orange/app/scripts/app.js
+++ public/orange/app/scripts/app.js
@@ -27,7 +27,8 @@ angular
 	.config(['$routeProvider', function ($routeProvider) {
 		$routeProvider
 			.when('/', {
-				templateUrl: 'views/home/home.html'
+				templateUrl: 'views/home/home.html',
+				controller: 'HomeCtrl'
 			})
 			.when('/dummy', {
 				templateUrl: 'views/home/dummy_product.html',
